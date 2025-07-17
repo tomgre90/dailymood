9dfab47571c87d4c57220385b5cafbb482020b66
tomgre90 <greber.thomas@gmail.com>
2025-07-17 17:33:26 +0200

    Refactor form submission handling and improve thank you message display

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-var form = document.getElementById('sheetdb-form');
-var thankYouMessage = document.getElementById('thank-you-message');
+const form = document.getElementById('sheetdb-form');
 
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
     e.preventDefault();
-    
-    fetch(form.action, {
-        method : "POST",
+
+    fetch("https://sheetdb.io/api/v1/0h0s4tq528dgw", {
+        method: "POST",
         body: new FormData(form),
-    }).then(
-        response => {
-            if (response.ok) {
-                form.style.display = 'none';
-                thankYouMessage.style.display = 'block';
-            } else {
-                alert("There was a problem with your submission. Please try again.");
-            }
-    }).catch(() => {
-        alert("There was a problem with your submission. Please try again.");
-    });
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        form.style.display = "none";
+        document.getElementById("thank-you-message").style.display = "block";
+    })
+    .catch(error => console.error("Error:", error));
 });
